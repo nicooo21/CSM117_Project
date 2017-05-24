626ce9c07d9609cabfc9242d19a3695c82a8713b
nicooo21 <nmagana2112@gmail.com>
2017-05-24 15:16:46 -0700

    Added ConnectionLost Function and second client for testing

--- Python_Server.py
+++ Python_Server.py
@@ -1,15 +1,16 @@
 from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import reactor
-from twisted.protocols.basic import LineReceiver
 import string, sys
+from twisted.protocols.basic import LineReceiver
 from twisted.internet.error import CannotListenError
 from twisted.internet.interfaces import IReactorTCP
 portNumber = 6789
-ipAddress = '127.0.0.1'
+ipAddress = '131.179.16.191'
 numQueues = 2
 
 class Server(LineReceiver):
-    
+    delimiter = '\n'
+
     def __init__(self, factory):
 
         self.factory = factory
@@ -18,13 +19,20 @@ class Server(LineReceiver):
 
 
     def connectionMade(self):
-        
-        print("Successfully connected with Player: " + str(self.numPlayer))
+
+        print("Successfully connected with Player " + str(self.numPlayer))
         print("Current Players List: " + str(self.factory.players))
 
+    def connectionLost(self, reason):
+        
+        self.factory.connections-=1
+        self.factory.players.remove(self)
+        print("The connection for player: " + str(self.numPlayer) + " was lost. " + str(self.factory.connections) + " are left")
+
+    def lineReceived(self, line):
+        print("Line Received from Player " + str(self.numPlayer) + '\n' + line)
 
 class ServerFactory(Factory):
-
 
     def __init__(self):
 
@@ -43,6 +51,7 @@ class ServerFactory(Factory):
         print("Number of Connections: " + str(self.connections))
 
         return Server(self)
+
 
 try:
 
